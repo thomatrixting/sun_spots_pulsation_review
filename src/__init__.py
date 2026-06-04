dec073547a0415b587c187a551f8320d9036a401
thomas <thomas.quamtum@gmail.com>
2026-06-04 11:57:52 -0500

    created a functional workflow to analize the osiclacion of each sunpot and created a 03Adata analisis notebook to test the workflow on 5 diferent sunspots close to no refiment if needed and the default values work fine with the data however is necesary to add warning for the cadence computing at it can fail as ins DS04

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,14 @@
 """Package entry point for the sun_spots_pulsation_review utilities."""
 
 from .utilities import make_cube
+from .sunspot_analysis import (
+    verify_cadence,
+    load_and_mask,
+    plot_calibration_frame,
+    compute_metrics,
+    save_metrics_csv,
+    plot_time_series,
+    plot_ffts_separate,
+    plot_ffts_combined,
+    save_animation,
+)
